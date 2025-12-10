19a997f43c3e8bfceb70fd2534c7a461c401939f
shivu <techshivu77@hotmail.com>
2025-12-10 16:38:08 +0530

    project setup done

---
+++ src/main/java/com/phasezero/catalog_service/PhaseZeroCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phasezero.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseZeroCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseZeroCatalogServiceApplication.class, args);
+	}
+
+}
