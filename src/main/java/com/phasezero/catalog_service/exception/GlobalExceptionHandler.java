0c47bc04a9f285a8f2ac63a7232d090ee2ce4b32
shivu <techshivu77@hotmail.com>
2025-12-10 22:59:31 +0530

    exception done

--- src/main/java/com/phasezero/catalog_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/phasezero/catalog_service/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,5 +17,20 @@ public class GlobalExceptionHandler {
 	    return ResponseEntity.status(HttpStatus.CONFLICT)
 	            .body(Map.of("error", exception.getMessage()));
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+	    String message = ex.getBindingResult()
+	            .getFieldErrors()
+	            .stream()
+	            .map(FieldError::getDefaultMessage)
+	            .findFirst()
+	            .orElse("Invalid input");
+
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+	            .body(Map.of("message", message));
+	}
+
 
 }
